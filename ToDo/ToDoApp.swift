b83837116aee048667428b15467ce0fb9462f883
Danielle Lewis <103973410+dlewisdev@users.noreply.github.com>
2024-01-13 23:19:17 -0500

    Initial Commit

---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by Danielle Lewis on 1/13/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
